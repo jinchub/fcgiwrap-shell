2e66a65d18bc4f06d2c4fb6754d3c82468648850
jcor <jc@jinchuang.localdomain>
2020-12-26 21:53:24 +0800

    新样式

---
+++ js/nav.js
@@ -0,0 +1,37 @@
+$(function(){
+    // nav收缩展开
+    $('.nav-item>a').on('click',function(){
+        if (!$('.nav').hasClass('nav-mini')) {
+            if ($(this).next().css('display') == "none") {
+                //展开未展开
+                $('.nav-item').children('ul').slideUp(300);
+                $(this).next('ul').slideDown(300);
+                $(this).parent('li').addClass('nav-show').siblings('li').removeClass('nav-show');
+            }else{
+                //收缩已展开
+                $(this).next('ul').slideUp(300);
+                $('.nav-item.nav-show').removeClass('nav-show');
+            }
+        }
+    });
+    //nav-mini切换
+    $('#mini').on('click',function(){
+        if (!$('.nav').hasClass('nav-mini')) {
+            $('.nav-item.nav-show').removeClass('nav-show');
+            $('.nav-item').children('ul').removeAttr('style');
+            $('.nav').addClass('nav-mini');
+        }else{
+            $('.nav').removeClass('nav-mini');
+        }
+    });
+});
+
+$(document).bind("contextmenu", function () { return false; });//禁止右键
+document.oncontextmenu = function () { return false; };
+document.onkeydown = function () {
+    if (window.event && window.event.keyCode == 123) {
+        event.keyCode = 0;
+        event.returnValue = false;
+        return false;
+    }
+};
